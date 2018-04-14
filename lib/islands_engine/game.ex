b9c3d706aea4072eaf06d399a1a731bc885e006a
Kosmas Chatzimichalis <kosmas@mach7x.com>
2018-04-14 15:50:41 +0200

    Add Set islands functionality

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -14,6 +14,9 @@ defmodule IslandsEngine.Game do
   def position_island(game, player, key, row, col) when player in @players, do:
     GenServer.call(game, {:position_island, player, key, row, col})
 
+  def set_islands(game, player) when player in @players, do:
+    GenServer.call(game, {:set_islands, player})
+
   def init(name) do
     player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
     player2 = %{name: nil,  board: Board.new(), guesses: Guesses.new()}
@@ -56,6 +59,21 @@ defmodule IslandsEngine.Game do
         {:reply, {:error, :invalid_island_type}, state_data}
     end
   end
+
+  def handle_call({:set_islands, player}, _from, state_data) do
+    board = player_board(state_data, player)
+    with {:ok, rules} <- Rules.check(state_data.rules, {:set_islands, player}),
+         true         <- Board.all_islands_positioned?(board)
+    do
+      state_data
+      |> update_rules(rules)
+      |> reply_success({:ok, board})
+    else
+      :error -> {:reply, :error, state_data}
+      false  -> {:reply, {:error, :not_all_islands_positioned}, state_data}
+    end
+  end
+
   defp player_board(state_data, player), do: Map.get(state_data, player).board
 
   defp update_player2_name(state_data, name), do:
