d62a18d9a2e226883b39dab51625607214e6b914
Kosmas Chatzimichalis <kosmas@mach7x.com>
2018-04-21 17:35:57 +0200

    Starting and Stopping Child Processes

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -4,6 +4,7 @@ defmodule IslandsEngine.Game do
   alias IslandsEngine.{Board, Coordinate, Guesses, Island, Rules}
 
   @players [:player1, :player2]
+  @timeout 60 * 60 * 24 * 1000 
 
   def start_link(name) when is_binary(name), do:
     GenServer.start_link(__MODULE__, name, name: via_tuple(name))
@@ -25,7 +26,11 @@ defmodule IslandsEngine.Game do
   def init(name) do
     player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
     player2 = %{name: nil,  board: Board.new(), guesses: Guesses.new()}
-    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
+    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}, @timeout}
+  end
+
+  def handle_info(:timeout, state_data) do
+    {:stop, {:shutdown, :timeout}, state_data}
   end
 
   def handle_call({:add_player, name}, _from, state_data) do
@@ -124,5 +129,7 @@ defmodule IslandsEngine.Game do
     end)
   end
 
-  defp reply_success(state_data, reply), do: {:reply, reply, state_data}
+  defp reply_success(state_data, reply) do
+     {:reply, reply, state_data, @timeout}
+  end
 end
